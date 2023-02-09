32b20d2f296e2b072617fce92fba766c27019943
Domask <domask2@mail.ru>
2023-02-09 23:02:24 +0300

    add styles headet/nav/btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
 const Header = () => (
-  <header className='header'>
-    <div className="header_main">
-      <div className="header_wrapper">
-        <div className="logo">
-         <img src="https://yanakhodkina.com/wp-content/themes/yanakhodkina_2/images/logo.svg"
-              alt=" | Сайт графического-дизайнера Веры Гордеевой"/>
-        </div>
-        <div className="branding">1</div>
-        <div className="header_menu">menu</div>
-        <div className="menu">---</div>
-      </div>
-    </div>
-  </header>
- )
+        <header className='header'>
+            <div className="header_main">
+                <div className="header_wrapper">
+                    <div className="logo">
+                        <img src="https://yanakhodkina.com/wp-content/themes/yanakhodkina_2/images/logo.svg"
+                             alt="Сайт графического-дизайнера Веры Гордеевой"/>
+                    </div>
+                    <div style={{display: 'none'}} className="branding">1</div>
+                    <div className="header_menu">
+                        <nav className="main_menu">
+                            <ul className="primary_menu">
+                                <li>Портфолио</li>
+                                <li>Услуги</li>
+                                <li>Информация</li>
+                                <li>Блог</li>
+                                <li>Лента</li>
+                                <li>Магазин</li>
+                            </ul>
 
+                            <button className='btn' type='button'>
+                                Контакты
+                            </button>
+                        </nav>
+
+                    </div>
+                    <div style={{display: 'none'}} className="menu_mobile">---aaa</div>
+                </div>
+            </div>
+        </header>
+    )
 export default Header;
