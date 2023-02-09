e894b341b94335cde7c8d085460086ee1b863308
Гордеев Сергей <domask1989@gmail.com>
2023-02-09 16:52:58 +0300

    new design

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Services from "./components/services/Services";
+import Header from "./components/Header/Header";
 
 const App = () => (
     <div className='app'>
+        <Header/>
         <Navbar/>
         <Intro/>
         <Services/>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => (
+  <header className='header'>
+    <div className="header_main">
+      <div className="header_wrapper">
+        <div className="logo">
+         <img src="https://yanakhodkina.com/wp-content/themes/yanakhodkina_2/images/logo.svg"
+              alt=" | Сайт графического-дизайнера Веры Гордеевой"/>
+        </div>
+        <div className="branding">1</div>
+        <div className="header_menu">menu</div>
+        <div className="menu">---</div>
+      </div>
+    </div>
+  </header>
+ )
+
+export default Header;
